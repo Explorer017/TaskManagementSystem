cdca2d9a3fc07ba2280b8377967ce23ee63aac6a
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2024-04-16 12:29:19 +0100

    more fixes to do with the empty get

--- php/components/ListList.php
+++ php/components/ListList.php
@@ -1,3 +1,5 @@
+<?php $ListIDInURL = isset($_GET['listid']) ? $_GET['listid'] : false;?>
+
 <ul class="nav nav-pills flex-column mb-auto overflow-y-auto">
     <li class="hstack gap-3 pb-3">
         <?php require 'components/NewListPopup.php'?>
@@ -7,13 +9,13 @@
     <h5>My Lists</h5>
     <?php
         foreach (getOwnedListsForCurrentUser() as $listID):
-            if($listID[0] == $_GET['listid']):?>
+            if($listID[0] == $ListIDInURL):?>
                 <li class="hstack gap-3 nav-link active text-white">
                     <div class="">
                         <?php echo getListNameFromID($listID[0])[0][0]?>
                     </div>
-                    <button class="btn btn-danger ms-auto" onclick="location.href='listAction.php?action=delete&listid=<?php echo $_GET["listid"]?>'"><i class="bi bi-trash"></i></button>
-                    <button class="btn btn-secondary"  onclick="location.href='modifyList.php?listid=<?php echo $_GET["listid"]?>'"><i class="bi bi-three-dots"></i></button>
+                    <button class="btn btn-danger ms-auto" onclick="location.href='listAction.php?action=delete&listid=<?php echo $ListIDInURL?>'"><i class="bi bi-trash"></i></button>
+                    <button class="btn btn-secondary"  onclick="location.href='modifyList.php?listid=<?php echo $ListIDInURL?>'"><i class="bi bi-three-dots"></i></button>
                 </li>
             <?php else:?>
                 <li class="hstack gap-3">
@@ -22,8 +24,8 @@
                             <?php echo getListNameFromID($listID[0])[0][0]?>
                         </a>
                     </div>
-                    <button class="btn btn-outline-danger ms-auto" onclick="location.href='listAction.php?action=delete&listid=<?php echo $_GET["listid"]?>'"><i class="bi bi-trash"></i></button>
-                    <button class="btn btn-outline-secondary" onclick="location.href='modifyList.php?listid=<?php echo $_GET["listid"]?>'"><i class="bi bi-three-dots"></i></button>
+                    <button class="btn btn-outline-danger ms-auto" onclick="location.href='listAction.php?action=delete&listid=<?php echo $ListIDInURL?>'"><i class="bi bi-trash"></i></button>
+                    <button class="btn btn-outline-secondary" onclick="location.href='modifyList.php?listid=<?php echo $ListIDInURL?>'"><i class="bi bi-three-dots"></i></button>
                 </li>
             <?php endif;?>
     <?php endforeach;?>
@@ -31,12 +33,12 @@
     <h5>Lists shared with me</h5>
     <?php
     foreach (getCollabListsForCurrentUser() as $listID):
-        if($listID[0] == $_GET['listid']):?>
+        if($listID[0] == $ListIDInURL):?>
             <li class="hstack gap-3 nav-link active text-white">
                 <div class="">
                     <?php echo getListNameFromID($listID[0])[0][0]?>
                 </div>
-                <button class="btn btn-danger ms-auto" onclick="location.href='listAction.php?action=leave&listid=<?php echo $_GET["listid"]?>'"><i class="bi bi-box-arrow-right"></i></button>
+                <button class="btn btn-danger ms-auto" onclick="location.href='listAction.php?action=leave&listid=<?php echo $ListIDInURL?>'"><i class="bi bi-box-arrow-right"></i></button>
             </li>
         <?php else:?>
             <li class="hstack gap-3">
@@ -45,7 +47,7 @@
                         <?php echo getListNameFromID($listID[0])[0][0]?>
                     </a>
                 </div>
-                <button class="btn btn-outline-danger ms-auto" onclick="location.href='listAction.php?action=leave&listid=<?php echo $_GET["listid"]?>'"><i class="bi bi-box-arrow-right"></i></button>
+                <button class="btn btn-outline-danger ms-auto" onclick="location.href='listAction.php?action=leave&listid=<?php echo $ListIDInURL?>'"><i class="bi bi-box-arrow-right"></i></button>
             </li>
         <?php endif;?>
     <?php endforeach;?>
