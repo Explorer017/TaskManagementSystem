2ed8353bb7b0092aeffd2d93322f6232778424b7
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2024-04-16 10:24:55 +0100

    Added collaborating lists to sidebar

--- php/components/ListList.php
+++ php/components/ListList.php
@@ -29,4 +29,24 @@
     <?php endforeach;?>
     <hr/>
     <h5>Lists shared with me</h5>
+    <?php
+    foreach (getCollabListsForCurrentUser() as $listID):
+        if($listID[0] == $_GET['listid']):?>
+            <li class="hstack gap-3 nav-link active text-white">
+                <div class="">
+                    <?php echo getListNameFromID($listID[0])[0][0]?>
+                </div>
+                <button class="btn btn-danger ms-auto" onclick="location.href='listAction.php?action=leave&listid=<?php echo $_GET["listid"]?>'"><i class="bi bi-box-arrow-right"></i></button>
+            </li>
+        <?php else:?>
+            <li class="hstack gap-3">
+                <div class="nav-link text-white">
+                    <a class="navbarList" href="<?php echo "index.php?listid=$listID[0]"?>">
+                        <?php echo getListNameFromID($listID[0])[0][0]?>
+                    </a>
+                </div>
+                <button class="btn btn-outline-danger ms-auto" onclick="location.href='listAction.php?action=leave&listid=<?php echo $_GET["listid"]?>'"><i class="bi bi-box-arrow-right"></i></button>
+            </li>
+        <?php endif;?>
+    <?php endforeach;?>
 </ul>
--- php/functions/DatabaseFunctions.php
+++ php/functions/DatabaseFunctions.php
@@ -39,6 +39,25 @@ function getOwnedListsForCurrentUser(){
     return $resultArray;
 }
 
+function getCollabListsForCurrentUser(){
+    $uid = getUIDFromCreds();
+
+    $db = new SQLite3("../db/database.db");
+    $SQL = "SELECT list_id FROM  UserLists WHERE user_id = :uid AND collaborator = 1 AND observer = 0";
+    $stmt = $db->prepare($SQL);
+
+    $stmt->bindValue(":uid", $uid, SQLITE3_TEXT);
+
+    $result = $stmt->execute();
+    while($row = $result->fetchArray()){
+        $resultArray [] = $row;
+    }
+    if (!isset($resultArray)){
+        return false;
+    }
+    return $resultArray;
+}
+
 function getListNameFromID($listID){
 
     $db = new SQLite3("../db/database.db");
